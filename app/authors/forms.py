43592f2816542914bf496ddc447195abcec18cec
ivan_grigorev <rauscral@gmail.com>
2020-04-30 00:38:09 +0300

    validators for repeat name author and book

--- app/authors/forms.py
+++ app/authors/forms.py
@@ -1,5 +1,13 @@
-from wtforms import Form, StringField, validators
+from wtforms import Form, StringField, validators, ValidationError
+from app.models import Author
 
 
 class AddAuthor(Form):
     first_name = StringField("Имя Автора", [validators.DataRequired()])
+
+    def validate_first_name(self, first_name):
+        author_count = Author.query.filter_by(first_name=first_name.data).count()
+        if author_count > 0:
+            raise ValidationError("Введенный вами автор уже существует")
+
+        
--- app/books/forms.py
+++ app/books/forms.py
@@ -1,5 +1,11 @@
-from wtforms import Form, StringField, validators
+from wtforms import Form, StringField, validators, ValidationError
+from app.models import Book
 
 
 class AddBook(Form):
     title = StringField("Название книги", [validators.DataRequired()])
+
+    def validate_title(self, title):
+        book_count = Book.query.filter_by(title=title.data).count()
+        if book_count > 0:
+            raise ValidationError("Введенный вами автор уже существует")
